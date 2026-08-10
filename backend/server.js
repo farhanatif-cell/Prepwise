849aabc57db2ab4c3cea2f2398fab15c097bd51b
Farhan <farhanatif51648@gmail.com>
2026-08-11 01:25:36 +0530

    backend

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+
+const app = express();
+
+
+// Connect MongoDB
+connectDB();
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Test route
+app.get("/", (req, res) => {
+  res.json({
+    message: "PrepWise API is running",
+  });
+});
+
+
+// Auth routes
+app.use("/api/auth", authRoutes);
+
+
+// Start server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`PrepWise backend running on port ${PORT}`);
+});
